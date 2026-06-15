14d774823ad57dad32e91fe99165e3b7dc09d975
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-15 16:05:33 -0400

    Agregada página new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'new',
+    loadComponent: () => import('./new/new.page').then( m => m.NewPage)
+  },
 ];
