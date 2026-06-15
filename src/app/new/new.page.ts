7c74ae9c60c4f90cd2200b0a877924f701ab7efb
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-15 16:13:26 -0400

    Agregados íconos a la vista new

--- src/app/new/new.page.ts
+++ src/app/new/new.page.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import { IonBackButton, IonButtons, IonContent, IonHeader, IonIcon, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { heart, logoIonic } from 'ionicons/icons';
 @Component({
   selector: 'app-new',
   templateUrl: './new.page.html',
   styleUrls: ['./new.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonIcon]
 })
 export class NewPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({heart, logoIonic});
+  }
 
   ngOnInit() {
   }
