b125f542f90b16f09b956bf6a28451edd701986e
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-15 16:18:28 -0400

    Agregada lista y botón de scroll

--- src/app/new/new.page.ts
+++ src/app/new/new.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonIcon, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { heart, logoIonic } from 'ionicons/icons';
 @Component({
@@ -9,15 +9,23 @@ import { heart, logoIonic } from 'ionicons/icons';
   templateUrl: './new.page.html',
   styleUrls: ['./new.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonIcon]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonIcon, IonItem, IonLabel, IonButton]
 })
 export class NewPage implements OnInit {
+
+  @ViewChild(IonContent) content!: IonContent;
+
+  items = Array.from({length: 50}, (_, i) => i);
 
   constructor() {
     addIcons({heart, logoIonic});
   }
 
   ngOnInit() {
+  }
+
+  public scrollToBottom(): void {
+    this.content.scrollToBottom(300);
   }
 
 }
